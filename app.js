f9bc57523c802e5b5078481f5f5a2642aca014ec
jaewxxkshin <jaewxxkshin@naver.com>
2024-11-21 11:31:08 +0900

    회원가입페이지 작업완료

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-
-// // 정적 파일 제공
+// 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 기본 경로 설정
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'login.html')); 
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'login.html')); 
 });
+
 // HTML 파일 응답
-app.get('/pages/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages','login.html'));
+app.get('/public/pages/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'login.html'));
+});
+
+app.get('/public/pages/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'signup.html'));
 });
 
 // 서버 시작
